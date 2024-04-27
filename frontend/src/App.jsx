316a595433c7cc2f4b9110b12a4abd7321717d57
Juan Gonzalez <jgonzalezjelvez@gmail.com>
2024-04-27 13:33:29 -0400

    corrección de errores

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,6 +21,14 @@ function App() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
+  const [newName, setNewName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+
+  const [findUser, setFindUser] = useState("");
+  const [idUser, setIdUser] = useState("");
+
+  const [selectedUser, setSelectedUser] = useState({});
+
   const createUser = () => {
     fetch("http://localhost:3000/api/usuarios/", {
       method: "POST",
@@ -30,6 +38,8 @@ function App() {
       if (res.ok) {
         console.log("usuario creado");
         setUpdate(true);
+        setName("");
+        setEmail("");
       }
     });
   };
@@ -53,10 +63,47 @@ function App() {
     }).then((res) => {
       if (res.ok) {
         console.log("Usuario editado");
-        setUsers(res.json());
         setUpdate(true);
+      } else {
+        throw console.error("Error al editar el usuario");
       }
     });
+  };
+
+  const searchUser = () => {
+    fetch(`http://localhost:3000/api/usuarios/${idUser}`, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length !== 0) {
+          setFindUser(data[0]);
+          setIdUser("");
+        } else {
+          throw console.error("Usuario no encontrado");
+        }
+      });
+  };
+
+  // console.log(newName);
+  // console.log(newEmail);
+
+  const handleOpenModal = (user) => {
+    setSelectedUser(user.id);
+    setNewName(user.name);
+    setNewEmail(user.email);
+    setIsOpen(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Llamar a la función editUser con los nuevos valores
+    editUser(selectedUser, newName, newEmail);
+    // Limpiar los campos después de editar
+    setNewName("");
+    setNewEmail("");
+    setSelectedUser("");
+    closeModal();
   };
 
   useEffect(() => {
@@ -72,9 +119,40 @@ function App() {
 
   //console.log(name);
   //console.log(email);
+  //console.log(findUser);
 
   return (
     <>
+      <div>
+        <h2>Buscar usuario por ID</h2>
+        <input
+          value={idUser}
+          onChange={(e) => setIdUser(e.target.value)}
+          type="number"
+        />
+        <button onClick={searchUser}>Buscar</button>
+        {Object.keys(findUser).length !== 0 && (
+          <div className="table-container">
+            <table>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Nombre</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{findUser.id}</td>
+                  <td>{findUser.name}</td>
+                  <td>{findUser.email}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
+
       <div className="create-container">
         <h1>Ingresar Usuario</h1>
         <input
@@ -110,7 +188,7 @@ function App() {
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>
-                  <button onClick={openModal}>Editar</button>
+                  <button onClick={() => handleOpenModal(user)}>Editar</button>
                   <button onClick={() => deleteUser(user.id)}>Eliminar</button>
                 </td>
               </tr>
@@ -124,11 +202,19 @@ function App() {
                 <button>X</button>
               </span>
               <h2>Editar Usuario</h2>
-              <input type="text" value={name} placeholder="Nombre" />
-              <input type="text" value={email} placeholder="Email" />
-              <button onClick={() => editUser(newEmail, newName, id)}>
-                Guardar Cambios
-              </button>
+              <input
+                type="text"
+                value={newName}
+                onChange={(e) => setNewName(e.target.value)}
+                placeholder="Nuevo Nombre"
+              />
+              <input
+                type="text"
+                value={newEmail}
+                onChange={(e) => setNewEmail(e.target.value)}
+                placeholder="Nuevo Email"
+              />
+              <button onClick={handleSubmit}>Guardar Cambios</button>
             </div>
           </div>
         )}
